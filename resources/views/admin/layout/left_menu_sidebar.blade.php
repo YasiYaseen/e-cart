532166365977f6b0217bd596a684d1e207583d3c
Yaseen <yasikalleri@gmail.com>
2024-03-28 12:21:25 +0530

    fix

--- resources/views/admin/layout/left_menu_sidebar.blade.php
+++ resources/views/admin/layout/left_menu_sidebar.blade.php
@@ -13,7 +13,7 @@
           <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Muhammed Yaseen</a>
+          <a href="#" class="d-block">{{auth('admin')->user()->name}}</a>
         </div>
       </div>
 
